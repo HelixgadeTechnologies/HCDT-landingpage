c44d94807c6a2c1d170f79c8540d49bf771873a5
estherchinda <davidakachi9@gmail.com>
2025-07-08 20:34:49 +0100

    replaced sdn text with sdn image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className=" flex flex-col md:flex-row md:items-center gap-y-5 justify-between max-w-7xl py-16 md:py-20 mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="font-medium text-base text-dark-1">
-        SDN{" "}
+      <div className="font-medium text-base text-dark-1 flex gap-1 items-end">
+        <Image
+        src={"/images/sdn-logo.jpeg"}
+        alt="SDN"
+        width={30}
+        height={30}
+        className="object-cover"
+        />
         <span className="text-base text-gray-1">
           @ 2025. All rights reserved.
         </span>
